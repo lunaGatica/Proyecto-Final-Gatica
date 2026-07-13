070a9ddfbf4273e68eb12b2f998d6e86f6e445b1
lunaGatica <89168675+lunaGatica@users.noreply.github.com>
2026-07-13 14:42:38 -0300

    Add files via upload

---
+++ Proyecto final/src/App.jsx
@@ -0,0 +1,24 @@
+import { ToastContainer } from 'react-toastify';
+import AppRouter from './routes/AppRouter';
+
+function App() {
+  return (
+    <>
+      <AppRouter />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </>
+  );
+}
+
+export default App;
